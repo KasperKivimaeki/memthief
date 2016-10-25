c9cc1df729fabc9bcb9294b58ebf1757ae20809f
KasperKivimaeki <lordi@paivola.fi>
2016-10-25 16:58:41 +0300

    Added comments

--- main.c
+++ main.c
@@ -4,31 +4,47 @@
 #include <inttypes.h>
 #include <string.h>
 
-unsigned long long getTotalSystemMemory()
-{
+// Reads the total amount of memory in the system.
+uint64_t getTotalSystemMemory() {
 		long pages = sysconf(_SC_PHYS_PAGES);
 		long page_size = sysconf(_SC_PAGE_SIZE);
 		return pages * page_size;
 }
 
-int main(int argc, char** argv)
-{
-		uint64_t realMem = getTotalSystemMemory();
-		int thief;
-		for(;;){
-				printf("How much memory do you want to reserve?\n");
-				printf("Example: 50\n\nReserve: ");
-				int scan = scanf("%d", &thief);
-				printf("%");
-				if(!scan){return -1;}
-				if(thief > 75){
-						printf("Don't steal it all!\n");
-				} else {
-						break;
-				}
+int main(int argc, char** argv) {
+		int limit;
+		if (argc != 1) {
+				limit = atoi(argv[1]);
+		} else {
+				limit = 75;
 		}
-		char *alpha = malloc(thief*((realMem)/100));
-		memset(alpha, 1, (thief/100.0)*realMem);
-		for(;;){sleep(1000);}
-		return 1;
+
+		// Total memory of the system.
+		uint64_t realMemory = getTotalSystemMemory();
+
+		// Amount of percentage points to be allocated
+		char size;
+
+		// Ask user for print
+		printf ("How much memory do you want to reserve?\n");
+		for(;;) {
+				// Read user input
+				int scan = scanf ("%d", &size);
+				// Prevent user from crashing the system
+				if (size < limit)
+						break;
+		}
+
+		printf ("Allocating memory\n");
+
+		// Allocate memory and fill it with ones
+		uint64_t beta = (realMemory * size) / 100;
+		memset( malloc (beta), 1, beta);
+
+		printf ("Memory allocated\n");
+
+		// Leave the program running
+		for(;;)
+				// Prevent it from wasting system resources
+				sleep (1000);
 }
